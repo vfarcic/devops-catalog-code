594fe29ba3eea20cfa7fac5194268ff8e8d94cb0
Joost van der Griendt <jvandergriendt@cloudbees.com>
2020-04-17 22:42:39 +0200

    add labels to storage
    I think this is just the nice thing to do, give people a bit more info about this bucket

--- terraform-gke/files/storage.tf
+++ terraform-gke/files/storage.tf
@@ -4,4 +4,9 @@ resource "google_storage_bucket" "state" {
   force_destroy = true
   project       = var.project_id
   storage_class = "NEARLINE"
+  labels        = {
+    environment = "development"
+    created-by  = "terraform"
+    owner       = "vfarcic"
+  }
 }
