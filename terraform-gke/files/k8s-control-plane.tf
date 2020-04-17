b66a30f94e40465d7b466329a4bb5d57dd78863c
Joost van der Griendt <jvandergriendt@cloudbees.com>
2020-04-17 22:40:12 +0200

    add resource labels
    Labels are will apply to the node configs, not what we want, so in this case its `resource_label`.
    
    I think most organizations would require this to be set - I know CloudBees does. So best to not ignore standard things.

--- terraform-gke/files/k8s-control-plane.tf
+++ terraform-gke/files/k8s-control-plane.tf
@@ -4,4 +4,10 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
   min_master_version       = var.k8s_version
+  
+  resource_labels          = {
+    environment            = "development"
+    created-by             = "terraform"
+    owner                  = "vfarcic"
+  }
 }
