f2813ff9374ce29ce0ce529997371f2ac5837359
Viktor Farcic <vfarcic@farcic.com>
2020-04-22 23:10:19 +0200

    Helm

---
+++ terraform-gke/minimal/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "gcs" {
+    bucket      = "devops-catalog"
+    prefix      = "terraform/state"
+    credentials = "account.json"
+  }
+}
+resource "google_container_cluster" "primary" {
+  name                     = var.cluster_name
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  min_master_version       = var.k8s_version
+  
+  resource_labels          = {
+    environment            = "development"
+    created-by             = "terraform"
+    owner                  = "vfarcic"
+  }
+}
+resource "google_container_node_pool" "primary_nodes" {
+  name         = var.cluster_name
+  location     = var.region
+  cluster      = "${google_container_cluster.primary.name}"
+  version      = var.k8s_version
+  node_config {
+    preemptible  = var.preemptible
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  autoscaling { 
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+  management {
+    auto_upgrade = false
+  }
+  timeouts {
+    create = "15m"
+    update = "1h"
+  }
+}
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = var.project_id
+  region      = var.region
+}
+resource "google_storage_bucket" "state" {
+  name          = "devops-catalog"
+  location      = var.region
+  project       = var.project_id
+  storage_class = "NEARLINE"
+  labels        = {
+    environment = "development"
+    created-by  = "terraform"
+    owner       = "vfarcic"
+  }
+}
---
+++ terraform-gke/minimal/output.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = var.cluster_name
+}
+
+output "region" {
+  value = var.region
+}
+
+output "project_id" {
+  value = var.project_id
+}
