d4e8c466e1634daeb0b06b2dccfff77a939e7548
Viktor Farcic <vfarcic@farcic.com>
2020-04-23 16:22:42 +0200

    Helm

--- terraform-aks/minimal/main.tf
+++ terraform-aks/minimal/main.tf
@@ -18,3 +18,17 @@ resource "azurerm_kubernetes_cluster" "primary" {
 provider "azurerm" {
   features {}
 }
+resource "azurerm_storage_account" "state" {
+  name                     = "devopscatalog"
+  resource_group_name      = var.resource_group
+  location                 = var.region
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "state" {
+  name                  = "devopscatalog"
+  storage_account_name  = azurerm_storage_account.state.name
+  container_access_type = "blob"
+}
+
