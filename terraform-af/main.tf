56cbdd169f5802240d24ff6f62f4fae3d5964e30
Viktor Farcic <vfarcic@farcic.com>
2020-06-29 01:40:05 +0200

    Azure Functions

--- terraform-af/main.tf
+++ terraform-af/main.tf
@@ -13,13 +13,4 @@ resource "azurerm_resource_group" "main" {
   location = var.region
 }
 
-# resource "azurerm_storage_account" "main" {
-#   name                     = "docserverless"
-#   resource_group_name      = azurerm_resource_group.main.name
-#   location                 = var.region
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   account_kind             = "Storage"
-# }
-
 data "azurerm_client_config" "main" { }
