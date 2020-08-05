248715c11554d426bb051776fc0c2a4e45384e59
Viktor Farcic <vfarcic@farcic.com>
2020-08-05 02:18:32 +0200

    Knative

--- terraform-aks/simple/main.tf
+++ terraform-aks/simple/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_kubernetes_cluster" "primary" {
 
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
-    command = "aks get-credentials --name ${var.cluster_name} --resource-group ${var.resource_group} --file $PWD/kubeconfig"
+    command = "az aks get-credentials --name ${var.cluster_name} --resource-group ${var.resource_group} --file $PWD/kubeconfig"
   }
   depends_on = [
     azurerm_kubernetes_cluster.primary,
