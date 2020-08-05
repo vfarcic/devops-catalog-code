6eae2c750476c8c3aee1f8eb15e14220eb96c33b
Viktor Farcic <vfarcic@farcic.com>
2020-08-05 02:07:50 +0200

    Knative

--- terraform-aks/simple/main.tf
+++ terraform-aks/simple/main.tf
@@ -2,6 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "main" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group != "" ? var.resource_group : "${random_string.main.result}"
   location = var.region
